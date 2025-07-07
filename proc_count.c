3b104da343083ac7a55571ac1caf6af62d81df04
JJ <xj120804@g.ucla.edu>
2025-07-06 17:17:59 -0700

    Update proc_count.c

--- proc_count.c
+++ proc_count.c
@@ -8,6 +8,12 @@ static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
 	// TODO: it's all yours
+	int count = 0; 
+	struct task_struct *p;
+
+	for_each_process(p) count++;
+
+	seq_printf(m, "%d\n", count);
 	return 0;
 }
 
